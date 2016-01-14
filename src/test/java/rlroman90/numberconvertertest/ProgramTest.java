1cc314380ae03aba7d6dc9ee6159100a93ec0247
Rich <rlroman.90@gmail.com>
2016-01-15 00:54:00 +1100

    Created first test to run app.

--- src/test/java/rlroman90/numberconvertertest/ProgramTest.java
+++ src/test/java/rlroman90/numberconvertertest/ProgramTest.java
@@ -1,1 +1,14 @@
-package rlroman90.numberconverter;
+package rlroman90.numberconvertertest;
+
+import org.junit.Test;
+import rlroman90.numberconverter.Program;
+import junit.framework.TestCase;
+
+public class ProgramTest extends TestCase {
+
+    public void testRunApp() {
+        Program.main(new String[0]);
+    }
+}
+
+
